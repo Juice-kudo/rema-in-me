539bb245331c6efd0be6c9a3377e26180e8a52df
Juice-kudo <hani6908@me.com>
2025-04-06 16:54:08 +0900

    모바일 달력 스타일과 가독성 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
+// tailwind.config.ts
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    "level-1",
+    "level-2",
+    "level-3",
+    "level-4",
+    "level-5",
   ],
   theme: {
     extend: {
